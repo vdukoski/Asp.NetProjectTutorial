98022f34dc7137d2fdc7645ba1d57dff7b705c70
vdukoski <42813799+vdukoski@users.noreply.github.com>
2018-11-06 22:28:23 +0100

    autoMappngCustomers

--- Vidly/Vidly/Dtos/CustomerDto.cs
+++ Vidly/Vidly/Dtos/CustomerDto.cs
@@ -19,7 +19,7 @@ namespace Vidly.Dtos
 
         public byte MembershipTypeId { get; set; }
 
-        [Min18YearsIfAMember]
+        //[Min18YearsIfAMember]
         public DateTime? BirthDate { get; set; }
     }
 }
